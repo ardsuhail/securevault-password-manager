2a51ff152016aefa42384268bac4dce1549c32be
Suhail <suhailjho139@gmail.com>
2025-10-31 15:08:23 +0530

    third commit

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -16,9 +16,7 @@ import mongoose from 'mongoose'
 
 const connectDB =async()=>{
     try {
-        const conn=await mongoose.connect(process.env.MONGODB_URI,{
-            useNewUrlParser:true,
-        })
+        const conn=await mongoose.connect(process.env.MONGODB_URI)
     } catch (error) {
         console.error(error.message)
         process.exit(1)
